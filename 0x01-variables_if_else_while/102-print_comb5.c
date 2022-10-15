6a9a4ddf3b1acad1af61deddd99171129d9a4898
Joseph Ampadu <jkampadujnr@gmail.com>
2022-10-15 01:35:42 +0000

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,16 +9,16 @@ int main(void)
 {
 	int c, d;
 
-	for (c = 0; c < 100; c++)
+	for (c = 0; c <= 99; c++)
 	{
-		for (d = c + 1; d < 100; d++)
+		for (d = c + 1; d <= 99; d++)
 		{
 			putchar(c / 10 + '0');
 			putchar(c % 10 + '0');
 			putchar(' ');
 			putchar(d / 10 + '0');
 			putchar(d % 10 + '0');
-			if (c < 99)
+			if (c < 98)
 			{
 				putchar(',');
 				putchar(' ');
