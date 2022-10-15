c4563118926832d2b8a98d4263b74d87d4c00830
Joseph Ampadu <jkampadujnr@gmail.com>
2022-10-15 01:09:14 +0000

    11. The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c, d, e;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (d = c + 1; d <= '9'; d++)
+		{
+			for (e = d + 1; e <= '9'; e++)
+			{
+				putchar(c);
+				putchar(d);
+				putchar(e);
+				if (c < '7')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
