5b0f96842a01d006e72e97fccdb12b5d91ac052f
Joseph Ampadu <jkampadujnr@gmail.com>
2022-10-18 21:40:44 +0000

    6. There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,8 +4,9 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
 
 #endif
