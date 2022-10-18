97bde2ece7a2c1c6cc5df5a8f92a4837515ecacd
Joseph Ampadu <jkampadujnr@gmail.com>
2022-10-18 20:49:34 +0000

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print lowercase English alphabets 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+	char *str = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(str[j]);
+		}
+		_putchar('\n');
+	}
+}
