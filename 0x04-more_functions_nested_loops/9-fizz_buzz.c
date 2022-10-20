f63396bf81abea50e057f6492039967286452126
Joseph Ampadu <jkampadujnr@gmail.com>
2022-10-20 21:41:32 +0000

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+
+/**
+ * main - Fizz-Buzz
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 && i % 5)
+			printf("%d", i);
+
+		putchar((i < 100) ? ' ' : '\n');
+	}
+
+	return (0);
+}
