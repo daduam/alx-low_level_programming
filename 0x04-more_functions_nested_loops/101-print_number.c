a2caeccc6111395a670748c0c3de4df825fc809e
Joseph Ampadu <jkampadujnr@gmail.com>
2022-10-20 22:25:55 +0000

    what?

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,12 +8,12 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 		n *= -1;
 	}
 
 	if (n / 10)
 		print_number(n / 10);
 
-	_putchar(n % 10 + '0');
+	_putchar(n % 10 + 48);
 }
