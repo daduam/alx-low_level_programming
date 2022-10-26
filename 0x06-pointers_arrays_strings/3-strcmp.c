6425ebb725868be7a6fa732ae088d22fa7580c39
Joseph Ampadu <jkampadujnr@gmail.com>
2022-10-26 21:14:09 +0000

    check all equal first

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,21 +9,9 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
-	{
-		if (*s1 < *s2)
-			return (-1);
-		else if (*s1 > *s2)
-			return (1);
+	for (; *s1 == *s2; s1++, s2++)
+		if (*s1 == '\0')
+			return (0);
 
-		s1++, s2++;
-	}
-
-	if (*s1 != '\0')
-		return (-1);
-
-	if (*s2 != '\0')
-		return (1);
-
-	return (0);
+	return ((*s1 < *s2) ? -1 : 1);
 }
