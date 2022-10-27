d6cae8f2d17e39deda828ea8e88974bda0cfb25c
Joseph Ampadu <jkampadujnr@gmail.com>
2022-10-27 23:00:47 +0000

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer
+ */
+void print_number(int n)
+{
+	unsigned int k;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		k = -n;
+	}
+	else
+	{
+		k = n;
+	}
+
+	if (k / 10)
+		print_number(k / 10);
+
+	_putchar((k % 10) + '0');
+}
