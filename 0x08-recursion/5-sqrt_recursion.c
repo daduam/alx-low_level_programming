08dd2d068748bb8cf038c33a3d094746b09a801f
Joseph Ampadu <jkampadujnr@gmail.com>
2022-11-02 05:55:12 -0700

    sqrt recursions

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _sqrt - recursively guesses the natural square root of @n
+ * @n: int
+ * @root: guess
+ * Return: natural square root of @n if it has one. -1 if otherwise
+ */
+int _sqrt(int n, int root)
+{
+	if (n == root * root)
+		return (root);
+
+	if (n / 2 == root)
+		return (-1);
+
+	return (_sqrt(n, root + 1));
+}
+
+/**
+ * _sqrt_recursion - computes the natural square root of @n
+ * @n: int
+ * Return: natural square root of @n if it has one. -1 if otherwise
+ */
+int _sqrt_recursion(int n)
+{
+	int root = 0;
+
+	if (n < 0)
+		return (-1);
+
+	if (n == 1)
+		return (1);
+
+	return (_sqrt(n, root));
+}
