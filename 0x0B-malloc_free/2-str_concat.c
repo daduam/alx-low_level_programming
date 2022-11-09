2704b5cdaca3c86da0dacb9cd232300a551856f1
Joseph Ampadu <jkampadujnr@gmail.com>
2022-11-09 22:56:41 +0000

    fix null

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,20 +15,20 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s;
-	int i = 0;
-	int n1 = strlen(s1);
-	int n2 = strlen(s2);
+	int i = 0, j;
+	int n1 = s1 == NULL ? 0 : strlen(s1);
+	int n2 = s2 == NULL ? 0 : strlen(s2);
 
 	s = malloc(sizeof(char) * (n1 + n2 + 1));
 
 	if (s == NULL)
 		return (NULL);
 
-	while (*s1 != '\0')
-		s[i++] = *s1++;
+	for (j = 0; j < n1; i++, j++)
+		s[i] = s1[j];
 
-	while (*s2 != '\0')
-		s[i++] = *s2++;
+	for (j = 0; j < n2; i++, j++)
+		s[i] = s2[j];
 
 	s[i] = '\0';
 
