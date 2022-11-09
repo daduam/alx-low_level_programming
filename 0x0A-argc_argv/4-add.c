88bfbaca6fa1d55c39dfe4ec71357b031bf2ea20
Joseph Ampadu <jkampadujnr@gmail.com>
2022-11-09 20:46:27 +0000

    correct add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char **argv)
 {
-	int i, num, sum = 0;
+	int i, j, num, sum = 0;
 
 	if (argc == 1)
 	{
@@ -19,13 +19,17 @@ int main(int argc, char **argv)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			num = atoi(argv[i]);
 
-			if (num == 0)
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				puts("Error");
-				return (1);
+				if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+				{
+					puts("Error");
+					return (1);
+				}
 			}
+
+			num = atoi(argv[i]);
 
 			sum += num;
 		}
