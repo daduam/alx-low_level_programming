ecad15950ef5ed9fc9902dfdd0661ba86446d1b7
Joseph Ampadu <jkampadujnr@gmail.com>
2022-11-09 22:27:15 +0000

    handle malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,10 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	s = malloc(size + 1);
+	s = malloc(sizeof(char) * size);
+
+	if (s == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
