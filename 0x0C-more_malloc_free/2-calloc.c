2899608a90bf43c05098deaae9037756c6d60f74
Joseph Ampadu <jkampadujnr@gmail.com>
2022-11-12 12:47:50 +0000

    calloc fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,15 +11,18 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	unsigned int i;
+
+	char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	p = malloc(size * nmemb);
 
-	if (p == NULL)
-		return (NULL);
+	if (p != NULL)
+		for (i = 0; i < (size * nmemb); i++)
+			p[i] = 0;
 
 	return (p);
 }
