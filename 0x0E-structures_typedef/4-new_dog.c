aebfdf404cbfc52fbcbc6f9b14e5a9c272036632
Joseph Ampadu <jkampadujnr@gmail.com>
2022-11-14 22:11:26 +0000

    copy name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include "stdlib.h"
+#include "string.h"
 
 /**
  * new_dog - Creates a new dog.
@@ -15,13 +16,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 
 	dog = malloc(sizeof(dog_t));
-
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = name;
+	dog->name = malloc(sizeof(name));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->name = strcpy(dog->name, name);
+
 	dog->age = age;
-	dog->owner = owner;
+
+	dog->owner = malloc(sizeof(owner));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = strcpy(dog->owner, owner);
 
 	return (dog);
 }
