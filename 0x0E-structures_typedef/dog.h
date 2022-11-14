10eed7fc8994f03f839d08847712068ab0aca901
Joseph Ampadu <jkampadujnr@gmail.com>
2022-11-14 20:52:05 +0000

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - Holds information on a dog
+ * @name: Name of the dog
+ * @owner: Name of the dog owner
+ * @age: Age of the dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif /* MAIN_H */
