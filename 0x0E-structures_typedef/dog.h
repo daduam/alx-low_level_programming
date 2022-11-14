826476d0be1bbd8559341d24a6600b2e452586ff
Joseph Ampadu <jkampadujnr@gmail.com>
2022-11-14 21:11:02 +0000

    init dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - Holds information on a dog
@@ -14,4 +14,6 @@ struct dog
 	float age;
 };
 
-#endif /* MAIN_H */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* DOG_H */
