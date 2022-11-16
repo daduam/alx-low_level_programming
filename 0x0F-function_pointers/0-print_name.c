12ebe4c4aa61e560680b599c67ef0082a0e446a3
Joseph Ampadu <jkampadujnr@gmail.com>
2022-11-16 18:50:34 +0000

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints the name passed as parameter.
+ *
+ * @name: char*
+ * @f: void (*f)(char *)
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
