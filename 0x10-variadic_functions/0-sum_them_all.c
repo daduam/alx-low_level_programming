299c2d88f8d3ac10c371d26e0fee0c47df964f5b
Joseph Ampadu <jkampadujnr@gmail.com>
2022-11-17 17:12:22 +0000

    remove n check

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,9 +13,6 @@ int sum_them_all(const unsigned int n, ...)
 	int sum;
 	unsigned int i;
 
-	if (n == 0)
-		return (0);
-
 	va_start(ap, n);
 	for (i = 0, sum = 0; i < n; i++)
 		sum += va_arg(ap, int);
