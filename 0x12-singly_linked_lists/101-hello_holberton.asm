de225c946d51d761d3e84a1f1a4af4bac84e729e
Joseph Ampadu <jkampadujnr@gmail.com>
2022-11-24 21:13:23 +0000

    hello holberton asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .data
+  msg: db "Hello, Holberton", 0xa, 0x0
+
+section .text
+  extern printf
+  global main
+
+main:
+  push rbp
+  mov rbp, rsp
+  mov edi, msg
+  mov eax, 0
+  call printf
+  mov eax, 0
+  pop rbp
+  ret
