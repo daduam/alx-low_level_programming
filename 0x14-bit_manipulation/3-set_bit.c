f3bf56b299a4c811d7131a8a4737ebcf325a764f
Joseph Ampadu <jkampadujnr@gmail.com>
2022-12-01 17:39:25 +0000

    clear bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,7 +3,7 @@
 /**
  * set_bit - Sets the value of a bit at a given index to 1.
  *
- * @n: int.
+ * @n: Address of number to update.
  * @index: Index, starting from 0, of the bit to be set.
  * Return: 1 on success, or -1 if an error occurred.
  */
