e7ee2fc4c2ad0deefcb62683f5ba97e2581d451e
Joseph Ampadu <jkampadujnr@gmail.com>
2023-02-02 18:51:04 +0000

    (0x1A) check if key already in hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,17 +11,28 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx;
-	hash_node_t *new;
+	hash_node_t *ptr;
 
 	if (!ht || !ht->array || !key || strlen(key) == 0 || !value)
 		return (0);
-	new = malloc(sizeof(hash_node_t));
-	if (!new)
-		return (0);
 	idx = key_index((unsigned char *)key, ht->size);
-	new->key = strdup(key);
-	new->value = strdup(value);
-	new->next = ht->array[idx];
-	ht->array[idx] = new;
+	ptr = ht->array[idx];
+	while (ptr)
+	{
+		if (strcmp(ptr->key, key) == 0)
+		{
+			free(ptr->value);
+			ptr->value = strdup(value);
+			return (1);
+		}
+		ptr = ptr->next;
+	}
+	ptr = malloc(sizeof(hash_node_t));
+	if (!ptr)
+		return (0);
+	ptr->key = strdup(key);
+	ptr->value = strdup(value);
+	ptr->next = ht->array[idx];
+	ht->array[idx] = ptr;
 	return (1);
 }
