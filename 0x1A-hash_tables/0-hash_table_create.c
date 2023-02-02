5744a313a3a7ab28eaeaeab2017050d253b9c61e
Joseph Ampadu <jkampadujnr@gmail.com>
2023-02-02 17:12:35 +0000

    (0x1A) add task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ *
+ * @size: Size of the array.
+ * Return: A pointer to the newly created hash table, or NULL
+ *         if something went wrong.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+
+	new = malloc(sizeof(hash_table_t));
+	if (new)
+	{
+		new->size = size;
+		new->array = malloc(sizeof(hash_node_t) * size);
+		if (new->array)
+			return (new);
+	}
+	return (NULL);
+}
